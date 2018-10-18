6eb6a66313795f8f4e85d74764324e7fadb05a01
n17dccn102 <44254583+n17dccn102@users.noreply.github.com>
2018-10-18 14:37:41 +0700

    Update ten.cpp

--- Lab 02/ten.cpp
+++ Lab 02/ten.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-
 using namespace std;
-
 int main()
 {
-	cout<<"*     *     *     *     *"<<endl;
-	cout<<"*  *  *   * * *   *  *  *"<<endl;
-	cout<<"*     *  *     *  *     *"<<endl;
-	return 0;
+	cout<<"*     *     *    *      *"<<endl;
+	cout<<"* *   *    * *   * *  * *"<<endl;
+	cout<<"*  *  *   *****  *   *  *"<<endl;
+	cout<<"*    **  *     * *      *"<<endl;
+	cout<<"*     * *       **      *"<<endl;
 }
