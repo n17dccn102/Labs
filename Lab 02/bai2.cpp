d9ab1abd3ab189eaecfa36ebacbb97f6d32f27a6
n17dccn102 <44254583+n17dccn102@users.noreply.github.com>
2018-10-25 16:16:44 +0700

    Update bai2.cpp

--- Lab 02/bai2.cpp
+++ Lab 02/bai2.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
 
+using namespace std;
+
+float Tien(float goc, int sonam, float lai)
+{
+	for(int i=1; i<=sonam; i++)
+	{
+		goc= goc* (float) (lai/100)+goc;
+	}
+	return goc;
+}
+
+int main()
+{
+	float goc;
+	int sonam;
+	float lai;
+	cout<<"\nNhap so tien goc: "; cin>>goc;
+	cout<<"\nNhap so nam gui: "; cin>>sonam;
+	cout<<"\nNhap lai: "; cin>>lai;
+	cout<<"\nKet qua la: "<<Tien(goc, sonam, lai);
+	return 0;
+}
