d49ff5ce5a53c5b32ad22c128cb39f5981d986a1
n17dccn102 <44254583+n17dccn102@users.noreply.github.com>
2018-10-25 16:19:10 +0700

    Update bai3.cpp

--- Lab 02/bai3.cpp
+++ Lab 02/bai3.cpp
@@ -1,1 +1,67 @@
+#include<iostream>
+#include<string.h>
 
+using namespace std;
+
+float Tong(float a, float b)
+{
+	return a+b;
+}
+
+float Hieu(float a, float b)
+{
+	return a-b;
+}
+
+float Tich(float a, float b)
+{
+	return a*b;
+}
+
+float Thuong(float a, float b)
+{
+	return a/b;
+}
+
+void Caculator(float a, float b)
+{
+	char x;
+	cout<<"\nEnter first number, operator, second number: ";
+	cin>>a>>x>>b;
+	switch(x)
+	{
+		case '+':
+		  {
+			cout<<"\n"<<a<<"+"<<b<<": "<<Tong(a, b)<<endl;
+			break;
+		  }
+		case '-':
+			{
+			cout<<"\n"<<a<<"-"<<b<<": "<<Hieu(a, b)<<endl;
+			break;
+			}
+		case '*':
+			{
+			cout<<"\n"<<a<<"*"<<b<<": "<<Tich(a, b)<<endl;
+			break;
+			}
+		case '/':
+			{
+			cout<<"\n"<<a<<"/"<<b<<": "<<Thuong(a, b)<<endl;
+			break;
+			}		
+	}
+}
+
+int main()
+{
+	float a, b;
+	char luachon;
+	do
+	{
+		 Caculator(a, b);
+		 cout<<"\nDo another (y/n)?:";
+		 cin>>(luachon);
+	}while(luachon=='y');
+	return 0;
+}
